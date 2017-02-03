4e27d73f893427961ec8fb00eb71279b6de0b0d3
DASTABLET\Trin <trintin7@gmail.com>
2017-02-03 21:03:45 +0700

    Products page update

--- products.php
+++ products.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
   </head>
 <body style="background-color: #f9f9f9; font-family: 'Source Sans Pro', sans-serif;">
+<?php include 'productsList.php'; ?>
 	<div class="container">
 		<div class="col-14"><img src="/Massivesoft/content/img/Massivesoft_logo_b.png" class="img-fluid" alt="Massivesoft_Logo"></div><br>
     <ul class="nav nav-pills nav-fill nav-justified">
@@ -14,10 +15,37 @@
   			<li class="nav-item"><a class="nav-link active" href="#">Products</a></li>
   			<li class="nav-item"><a class="nav-link" href="shop.php">Shop</a></li>
 		</ul><hr>
-    <div class="col">
-        <br><h2 style="text-align: center;">Some stuff will also go here aswell,  Check out the <a href="shop.php">Shop</a> page in the meantime... </h2><br>
     </div>
-	</div>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <p style='text-align:middle;'>Some stuff will also go here aswell,  Check out the <strong><a href="shop.php">Shop</a></strong> page in the meantime...</p>
+      </div>
+<?php echo "  
+      <div class='container-fluid'>
+        <div class='well well-sm'>
+          <h1 style='text-align:left;'>Products</h1>
+          </div>
+        <div class='row'>";
+          for ($i=0 ; $i < 6; $i++) 
+          { 
+  echo " <div class='col center-block'>    
+              <div class='card' style='width: 21rem;'>
+                <div class='card-header'><span class='badge badge-pill badge-default'>".$product[$i][1]."</span></div>
+                  <img class='card-img-top img-fluid' src='".$product[$i][3]."' alt='".$product[$i][2]."'>
+                  <div class='card-block'>
+                    <h3 class='card-title'>".$product[$i][0]."</h3>
+                    <p class='card-text'>".$product[$i][4]."</p>
+                    <h3 style='text-align: right;''>$".$product[$i][5]."</h3>
+                  </div>
+                </div>
+                <br>
+              </div>            
+            ";
+          };
+  echo "        </div>";?>
+	</div></div>
 
   <?php include 'footer.php';?>
 </body>
